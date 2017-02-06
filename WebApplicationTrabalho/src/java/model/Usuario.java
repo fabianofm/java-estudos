8f08f092ca1c199ec2afe08edaf954782f0996e1
fabia <fabia@inspiron3421>
2017-02-06 20:06:56 -0200

    add ServicosOpicionais Model and DAO

--- WebApplicationTrabalho/src/java/model/Usuario.java
+++ WebApplicationTrabalho/src/java/model/Usuario.java
@@ -21,12 +21,14 @@ import javax.persistence.TemporalType;
  * Classe utilizada para representar uma Usuario.
  */
 @Entity
+/*
 @NamedQueries({
     @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
     @NamedQuery(name = "Usuario.findById", query = "SELECT u FROM Usuario u WHERE u.id = :id"),
     @NamedQuery(name = "Usuario.findByLogin", query = "SELECT u FROM Usuario u WHERE u.login = :login"),
     @NamedQuery(name = "Usuario.findByNome", query = "SELECT u FROM Usuario u WHERE u.nome = :nome"),
     @NamedQuery(name = "Usuario.findBySenha", query = "SELECT u FROM Usuario u WHERE u.senha = :senha")})
+*/
 public class Usuario implements Serializable {
 
     @Id 
